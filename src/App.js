5a20af45ae37ee35f23e8ca6c5d74c9ff7bc1c73
MissLiner <miss.liner@yahoo.com>
2022-02-13 20:31:54 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-useless-constructor */
+import React, { Component } from 'react';
+import Overview from './/components/Overview';
+import uniqid from 'uniqid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: { text: '',
+              id: uniqid(), 
+              index: 0,
+            },
+      tasks: [],
+    };
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  deleteItem = (e) => {
+    const delID = e.target.dataset.id;
+    let delIndex;
+    let newArray = this.state.tasks;
+    for(let item of newArray) {
+      if(item.id === delID) {
+        delIndex = this.state.tasks.indexOf(item);
+        return;
+      }
+    }
+    newArray.splice(delIndex, 1);
+    this.setState({
+      tasks: newArray
+    })
+  }
+  handleChange = (e) => {
+    this.setState({ 
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+        index: this.state.task.index,
+      }
+    });
+  };
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: '',
+              id: uniqid(),
+              index: this.state.tasks.length + 1,
+            },
+    });
+  };
+
+  render() {
+    const { task, tasks } = this.state;
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">New task:</label>
+          <input 
+            onChange={this.handleChange}
+            value={task.text} 
+            type="text" 
+            id="taskInput"   
+          />
+          <button type="submit">Add Task</button>
+        </form>
+        <Overview items={tasks} deleteFunc={this.deleteItem}/>
+      </div>
+    );
+  }
 }
 
 export default App;
